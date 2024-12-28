4a8f11b9efd059161b32bbf44582a790ee66d7d5
Andrew McIntosh <nacho.vonkickbutt@gmail.com>
2024-12-28 16:01:27 -0500

    Fix issues where events aren't shown for the next month
    Also handle multi-day events showing the day after they end

--- dataHelper.py
+++ dataHelper.py
@@ -24,7 +24,7 @@ def sort_by_date(e: Event):
 def get_events(max_number: int) -> List[Event]:
     events = []
     for calendar_url in settings.CALENDAR_URLS:
-        events += get_webdav_events(calendar_url, max_number)
+        events.extend(get_webdav_events(calendar_url, max_number))
     events.sort(key=sort_by_date)
     return events[:max_number]
 
@@ -33,6 +33,7 @@ def get_webdav_events(url: str, max_number: int) -> List[Event]:
     logger.info("Retrieving calendar infos")
     utc_timezone = tz.tzutc()
     current_timezone = tz.tzlocal()
+    today_midnight = datetime.now(current_timezone).replace(hour=0, minute=0, second=0, microsecond=0)
     is_apple = "icloud" in url
     try:
         event_list = events(url, fix_apple=is_apple)
@@ -42,19 +43,12 @@ def get_webdav_events(url: str, max_number: int) -> List[Event]:
         for event in event_list:
             event.start.replace(tzinfo=utc_timezone)
             event.start = event.start.astimezone(current_timezone)
-
-            # remove events from previous day (problem based on time-zones)
-            day_number = time.localtime().tm_mday
-            event_date = event.start.date()
-            if (day_number == 1 and event_date.month < time.localtime().tm_mon):
+            # Multi-day events end at midnight of the previous/current
+            # day and thus show up after they're over.
+            if event.end == today_midnight:
                 start_count += 1
                 max_number += 1
-            elif (event_date.day < day_number):
-                start_count += 1
-                max_number += 1
-
-        logger.info("Got {} calendar-entries (capped to {})".format(
-            len(event_list), max_number-start_count))
+        logger.info("Got %s calendar-entries (capped to %s)", len(event_list), max_number-start_count)
         return event_list[start_count:max_number]
 
     except Exception as e:
